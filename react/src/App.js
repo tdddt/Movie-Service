ad8c3307ac0aea1978e917b01e5bb7c9b6a563ec
tdddt <doto1116@ewhain.net>
2022-01-23 22:58:27 +0900

    seperate components, make css for that

--- react/src/App.js
+++ react/src/App.js
@@ -1,7 +1,11 @@
+import Button from "./Button.js";
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div>
-      <h1>Welcom back!</h1>
+      <h1 className={styles.title}>Welcom back!</h1>
+      <Button text="Continue"/>
     </div>
   );
 }
